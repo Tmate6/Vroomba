be073ae46edbce447fd9e5907c4233c9869cec65
Mate Tohai <admin@Tmate6.com>
2024-02-17 18:34:54 +0200

    add SD card

--- tests/lidarScan/lidarScan.ino
+++ tests/lidarScan/lidarScan.ino
@@ -1,15 +1,44 @@
+// SD
+#include "FS.h"
+#include "SD.h"
+#include "SPI.h"
+
+const byte SdPin = 5;
+
+
+// Magnetometer
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_HMC5883_U.h>
 
 Adafruit_HMC5883_Unified mag = Adafruit_HMC5883_Unified(12345);
 
+int startX;
+int startY;
+int startZ;
+
+
+// LiDAR
 #include "soc/soc.h"
 #include "soc/rtc_cntl_reg.h"
 
 #include <SoftwareSerial.h>
-SoftwareSerial LidarSerial(34, 19); // RX, TX
+SoftwareSerial LidarSerial(16, 17); // RX, TX
 
+int dist;
+int strength;
+byte temp;
+
+int check;
+int lidarReadLoop;
+
+int uart[9];
+const int HEADER=0x59;
+
+unsigned int scan[600];
+
+
+// Motor
 const byte rightDisable = 26;
 const byte leftDisable = 25;
 
@@ -18,24 +47,7 @@ const byte rightBack = 14;
 const byte leftForward = 32;
 const byte leftBack = 33;
 
-int dist;
-int strength;
-byte temp;
-
-int check;
-int i;
-
-int uart[9];
-const int HEADER=0x59;
-
-const int cyclesInFullTurn = 500;
-
-unsigned int scan[cyclesInFullTurn];
-
-int startX;
-int startY;
-int startZ;
-
+// Funcs
 void setFullRotation() {
   sensors_event_t event;
   mag.getEvent(&event);
@@ -48,16 +60,37 @@ void setFullRotation() {
 bool checkFullRotation() {
   sensors_event_t event;
   mag.getEvent(&event);
-/*
-  Serial.print(startX); Serial.print(" "); Serial.println(event.magnetic.x);
-  Serial.print(startY); Serial.print(" "); Serial.println(event.magnetic.y);
-  */
+
+  Serial.print(event.magnetic.x); Serial.print(" "); Serial.print(startX); Serial.print(" "); Serial.print(event.magnetic.y); Serial.print(" "); Serial.println(startY);
+  
   if (event.magnetic.x > (startX - 4) && event.magnetic.x < (startX + 4)) {
     if (event.magnetic.y > (startY - 4) && event.magnetic.y < (startY + 4)) {
       return true;
     }
   }
   return false;
+}
+
+void saveScan() {
+  if (SD.remove("/scan.txt")) {
+    Serial.println("File deleted");
+  } else {
+    Serial.println("Delete failed");
+  }
+  
+  File file = SD.open("/scan.txt", FILE_WRITE);
+
+  if (!file) {
+    Serial.println("Failed to open file");
+    return;
+  }
+
+  for (int i = 0; i < 600; i++) {
+    file.print(scan[i]);
+    file.print(",");
+  }
+  
+  file.close();
 }
 
 void scanLidar() {
@@ -75,7 +108,7 @@ void scanLidar() {
       break;
     }
     
-    Serial.println(readLidar());
+    scan[turns] = readLidar();
     turns++;
   }
 
@@ -83,6 +116,8 @@ void scanLidar() {
   digitalWrite(leftDisable, HIGH);
   digitalWrite(leftBack, LOW);
   digitalWrite(rightForward, LOW);
+
+  saveScan();
 }
 
 int readLidar() {
@@ -94,8 +129,8 @@ int readLidar() {
         if (LidarSerial.read() == HEADER) {
           uart[1] = HEADER;
             
-          for (i = 2; i < 9; i++) {
-            uart[i] = LidarSerial.read();
+          for (lidarReadLoop = 2; lidarReadLoop < 9; lidarReadLoop++) {
+            uart[lidarReadLoop] = LidarSerial.read();
           }
           
           check = uart[0] + uart[1] + uart[2] + uart[3] + uart[4] + uart[5] + uart[6] + uart[7];
@@ -121,14 +156,6 @@ void setup() {
   WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0);
 
   Serial.begin(115200);
-  
-  // Magnetometer
-  if (!mag.begin()) {
-    Serial.println("Magnetometer error");
-    while(1);
-  }
-  
-  Serial.println("Magnetometer working");
 
   // Motor
   pinMode(rightDisable, OUTPUT); digitalWrite(rightDisable, HIGH);
@@ -139,8 +166,29 @@ void setup() {
   pinMode(leftForward, OUTPUT); digitalWrite(leftForward, LOW);
   pinMode(leftBack, OUTPUT); digitalWrite(leftBack, LOW);
 
+  // Magnetometer
+  if (!mag.begin()) {
+    Serial.println("Magnetometer error");
+    while (1);
+  }
+  
+  Serial.println("Magnetometer initialised");
+
   // Lidar
   LidarSerial.begin(115200);
+
+  // SD
+  if (!SD.begin(SdPin)) {
+    Serial.println("SD card module error");
+    while (1);
+  }
+
+  if (SD.cardType() == CARD_NONE) {
+    Serial.println("SD card not found");
+    while (1);
+  }
+
+  Serial.println("SD card initialised");
 }
 
 void loop() {
